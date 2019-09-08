20cd7c9dd8bba604165ee7db2dff20ca41653788
Alex Nunes <alexandrenunesmartins@hotmail.com>
2019-09-08 11:56:06 -0300

    Implementacao basica Clientes e enderecos

--- src/main/java/com/alex/cursomc/domain/Estado.java
+++ src/main/java/com/alex/cursomc/domain/Estado.java
@@ -20,7 +20,7 @@ public class Estado  implements Serializable {
 	private String nome;
 	
 	@OneToMany(mappedBy = "estado")
-	private List<Cidade> cidades = new ArrayList();
+	private List<Cidade> cidades = new ArrayList<Cidade>();
 	
 	public Estado() {
 	}
